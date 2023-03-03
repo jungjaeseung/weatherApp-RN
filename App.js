4b660432f7843e2a2a32a12e0bd7824c3b8af6f3
3Harang <jungjaeseung9475@gmail.com>
2023-03-03 23:55:36 +0900

    get APIs of location, weather

--- App.js
+++ App.js
@@ -1,14 +1,62 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, Dimensions, StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import {
+  ScrollView,
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "513e9a0a6d518f61c20b93ff47f79249";
+
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(
+      json.list.filter((weather) => {
+        if (weather.dt_txt.includes("00:00:00")) {
+          return weather;
+        }
+      })
+    );
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.city}>
-        <Text style={styles.cityName}>Yongin</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         horizontal
@@ -16,26 +64,29 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator size="large" color="#fff" />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.date}>
+                {new Date(day.dt * 1000).toString().substring(0, 10)}
+              </Text>
+              <View style={styles.tempCnt}>
+                <Text style={styles.temp}>
+                  {parseFloat(day.main.temp).toFixed(1)}
+                </Text>
+                <Text style={styles.tempEnd}>℃</Text>
+              </View>
+              <Text style={styles.weatherMain}>{day.weather[0].main}</Text>
+              <Text style={styles.description}>
+                {day.weather[0].description}
+              </Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -60,12 +111,27 @@ const styles = StyleSheet.create({
     width: SCREEN_WIDTH,
     alignItems: "center",
   },
-  temp: {
+  date: {
     marginTop: 50,
-    fontSize: 160,
+    marginBottom: -10,
+    fontSize: 26,
+  },
+  temp: {
+    fontSize: 120,
+  },
+  tempCnt: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+  },
+  tempEnd: {
+    fontSize: 80,
+    marginLeft: 10,
+  },
+  weatherMain: {
+    marginTop: -10,
+    fontSize: 48,
   },
   description: {
-    marginTop: -30,
-    fontSize: 48,
+    fontSize: 32,
   },
 });
